16f33c1d6301e6e38c231b7fab718b750abd9f2f
Antonio Costas <acostass03@educantabria.es>
2023-05-03 19:10:06 +0200

    PAra ver con victor

---
+++ src/main/java/Ejer1/Main.java
@@ -0,0 +1,54 @@
+package Ejer1;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> numeros = leeValores();
+        int media = calcularSuma(numeros) / numeros.size();
+        mostrarResultado(numeros, calcularSuma(numeros), media);
+    }
+
+    public static void mostrarResultado(ArrayList<Integer> numeros, int suma, int mediaAritmetica) {
+        System.out.println("Todos los valores mostrados por pantalla: " + numeros +
+                "\nLa suma de todos los números es: " + suma +
+                "\nSu media aritmética es: " + mediaAritmetica +
+                "\nSon mayores a la media aritmética: ");
+        for (Integer mayorMedia : numeros) {
+            if (mayorMedia > mediaAritmetica) {
+                System.out.println(mayorMedia);
+            }
+        }
+
+    }
+
+    public static int calcularSuma(ArrayList<Integer> numeros) {
+        int total = 0;
+        Iterator<Integer> iterador = numeros.iterator();
+        while (iterador.hasNext()) {
+            total += iterador.next();
+        }
+        // Esto podría estar mejor,pero suda:
+        // iterador.remove();
+        return total;
+    }
+
+
+    public static ArrayList<Integer> leeValores() {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> valores = new ArrayList<>();
+        int valor;
+        do {
+            System.out.print("Introduce un número (-99 para terminar): ");
+            valor = scanner.nextInt();
+            if (valor != -99) {
+                valores.add(valor);
+            }
+        } while (valor != -99);
+        return valores;
+    }
+}
+
+
---
+++ src/main/java/Ejer2/Persona.java
@@ -0,0 +1,74 @@
+package Ejer2;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+public class Persona {
+    private String nombre, dni;
+    private LocalDate fechaNacimiento;
+
+    public Persona(String nombre, String dni, LocalDate fechaNacimiento) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    // Métodos para asegurarse de que sean correctos o no
+    // Aquí te mira si contiene letras mayúsculas y minúsculas como mínimo 3 sin máximo, en caso de ser menor salta error
+    public boolean validarNombre() {
+        return nombre.matches("[a-zA-Z0-9]{3,}");
+    }
+
+    // validar fecha, en caso de saltar la excepción retornará falso
+    public boolean validarFecha() {
+        try {
+            LocalDate.parse(fechaNacimiento.toString());
+            return true;
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+    }
+
+    // validar DNI:
+    public boolean validarDNI() {
+        // Comprobar que el DNI tiene nueve caracteres
+        if (dni.length() != 9) {
+            return false;
+        }
+
+        // Comprobar que los ocho primeros caracteres son números
+        if (!dni.substring(0, 8).matches("[0-9]{8}")) {
+            return false;
+        }
+
+        // Calcular la letra del DNI y comprobar que es correcta
+        int numeroDNI = Integer.parseInt(dni.substring(0, 8));
+        int resto = numeroDNI % 23;
+        char letraDNI = "TRWAGMYFPDXBNJZSQVHLCKE".charAt(resto);
+        return dni.charAt(8) == letraDNI;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+}
