63bf965a45b4f3e6a991721a22efe86cfec52569
Antonio Costas <acostass03@educantabria.es>
2023-05-04 23:35:51 +0200

    Comento la asociación por lo que sea

--- src/main/java/Ejer2/Principal.java
+++ src/main/java/Ejer2/Principal.java
@@ -6,6 +6,6 @@ public class Principal {
     public static void main(String[] args) {
         Persona per1;
         per1 = new Persona("Antonio Costas Salazar", "72228550V", LocalDate.of(2002, 05, 24));
-        Asociacion asoci = new Asociacion();
+        // Asociacion asoci = new Asociacion();
     }
 }
