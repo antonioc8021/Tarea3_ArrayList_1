b67e66eb2cf8da1483803558e92bbd0db07a35e5
Antonio Costas <acostass03@educantabria.es>
2023-05-04 21:58:45 +0200

    Creación de la clase Principal

--- src/main/java/Ejer2/Asociacion.java
+++ src/main/java/Ejer2/Asociacion.java
@@ -6,8 +6,12 @@ import java.util.List;
 public class Asociacion {
     List<Persona> personas=new ArrayList<>();
 
-    //
+    // Constructor por defecto:
     public Asociacion(List<Persona> personas) {
         this.personas = personas;
     }
+
+
+
+
 }
