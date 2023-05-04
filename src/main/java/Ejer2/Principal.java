45caa4dd7bc2c5b09dfe36ebc9538710dcc8715d
Antonio Costas <acostass03@educantabria.es>
2023-05-04 23:45:22 +0200

    Pruebas en mi casa

--- src/main/java/Ejer2/Principal.java
+++ src/main/java/Ejer2/Principal.java
@@ -7,6 +7,6 @@ public class Principal {
         Persona per1;
         per1 = new Persona("Antonio Costas Salazar", "72228550V", LocalDate.of(2002, 05, 24));
         // Asociacion asoci = new Asociacion(Todo: aquí debería de meter algo lo cual no se lo que es);
-        // aquí meto mas código para probar
+        // aquí meto mas código para probar por que si
     }
 }
