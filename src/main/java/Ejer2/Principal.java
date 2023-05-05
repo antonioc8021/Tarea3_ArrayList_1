248a62c8d8e05b88e0d83ac8234b3686dbee421a
Antonio Costas <acostass03@educantabria.es>
2023-05-05 12:27:28 +0200

    Creo un objeto de asociación con parámetro y otro sin parámetro

--- src/main/java/Ejer2/Principal.java
+++ src/main/java/Ejer2/Principal.java
@@ -7,8 +7,9 @@ import java.util.List;
 public class Principal {
     public static void main(String[] args) {
         Persona per1 = new Persona("Antonio Costas Salazar", "72228550V", LocalDate.of(2002, 5, 24));
-        List<Persona> personas=new ArrayList<>();
+        List<Persona> personas = new ArrayList<>();
         personas.add(per1);
-        Asociacion asoci = new Asociacion(personas);
+        Asociacion asociacion1 = new Asociacion(personas);
+        Asociacion asociacion2 = new Asociacion();
     }
 }
