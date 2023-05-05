28f20d7153d39afd3e7c0cfc7a090360e1824ba3
Antonio Costas <acostass03@educantabria.es>
2023-05-05 11:45:02 +0200

    Tal y como lo tengo debo de poner un array de personas (puede estar mejor)

--- src/main/java/Ejer2/Principal.java
+++ src/main/java/Ejer2/Principal.java
@@ -1,12 +1,14 @@
 package Ejer2;
 
+import java.lang.reflect.Array;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Principal {
     public static void main(String[] args) {
-        Persona per1;
-        per1 = new Persona("Antonio Costas Salazar", "72228550V", LocalDate.of(2002, 05, 24));
-        // Asociacion asoci = new Asociacion(Todo: aquí debería de meter algo lo cual no se lo que es);
-        // esto es algo simple pero muy útil
+        Persona per1 = new Persona("Antonio Costas Salazar", "72228550V", LocalDate.of(2002, 05, 24));
+        ArrayList <Persona> personas=new ArrayList<>();
+        personas.add(per1);
+        Asociacion asoci = new Asociacion(personas);
     }
 }
