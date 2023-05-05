fcf9835814a3546c12c10545f88eb70a9de30499
Antonio Costas <acostass03@educantabria.es>
2023-05-05 12:20:41 +0200

    Creo una lista para pasarla como parámetro

--- src/main/java/Ejer2/Principal.java
+++ src/main/java/Ejer2/Principal.java
@@ -3,11 +3,12 @@ package Ejer2;
 import java.lang.reflect.Array;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Principal {
     public static void main(String[] args) {
         Persona per1 = new Persona("Antonio Costas Salazar", "72228550V", LocalDate.of(2002, 05, 24));
-        ArrayList <Persona> personas=new ArrayList<>();
+        List<Persona> personas=new ArrayList<>();
         personas.add(per1);
         Asociacion asoci = new Asociacion(personas);
     }
